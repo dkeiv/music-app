a6d6101b3e30c91ddfde49ea09b34f56a548e03c
keiv <vudangkhoi1994@gmail.com>
2024-09-06 13:51:54 +0700

    start kit

---
+++ src/main/java/app/local/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package app.local.config;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity(debug = true)
+@EnableMethodSecurity
+@AllArgsConstructor
+public class SecurityConfig {
+    private final AuthenticationProvider authenticationProvider;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        auth -> auth
+                                .requestMatchers(
+                                        "/"
+                                ).permitAll()
+                                .anyRequest().authenticated()
+                )
+                .authenticationProvider(authenticationProvider)
+//                .sessionManagement(sess -> sess
+//                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // No sessions
+//                )
+//                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+        ;
+        return http.build();
+    }
+}
---
+++ src/test/java/app/local/MusicAppApplicationTests.java
@@ -0,0 +1,13 @@
+package app.local;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
