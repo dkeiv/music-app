cb5b3f829dbddd7940f80e58b453852fa9f95b1f
keiv <vudangkhoi1994@gmail.com>
2024-09-15 11:13:34 +0700

    update song admin controller

--- src/main/java/app/local/song/SongAdminController.java
+++ src/main/java/app/local/song/SongAdminController.java
@@ -2,51 +2,32 @@ package app.local.song;
 
 import app.local.artist.Artist;
 import app.local.artist.ArtistService;
-import app.local.exception.NotFoundException;
-import app.local.user.User;
 import app.local.user.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.FileCopyUtils;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.List;
-import java.util.Optional;
-
 
 @Controller
-@RequestMapping("/music-app/songs")
+@RequestMapping("/music-app/admin/songs")
 @RequiredArgsConstructor
-public class SongController {
-
+public class SongAdminController {
     private final SongService songService;
     private final UserService userService;
     private final ArtistService artistService;
-
-    @GetMapping
-    public String list(Model model,@PageableDefault(size = 10) Pageable pageable) {
-        Page<Song> allSongs = songService.findAll(pageable);
-        model.addAttribute("allSongs", allSongs);
-        return "song/index";
-    }
 
     @GetMapping("/{id}/view")
     public String viewSong() {
         return "song/detail";
     }
+
 
 
     @GetMapping("/create")
@@ -81,12 +62,7 @@ public class SongController {
 
     @PostMapping("/delete")
     public String delete() {
-       return "/";
+        return "/";
     }
 
-    @PostMapping("/like/{songId}")
-    public String likeSong(@PathVariable Long songId, @AuthenticationPrincipal User user) throws NotFoundException {
-        userService.likeSong(user.getId(), songId);
-        return "redirect:/music-app/songs" + songId;
-    }
 }
